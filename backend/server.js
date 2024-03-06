3a7bb7e81d61dc4d07aecbdced5476b5b758a766
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-03-06 13:53:35 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require("express");
-const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes")
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware")
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
 
 //using port number from .env file
 const PORT = process.env.PORT || 5000;
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id == req.params.id);
-  res.send(singleChat);
-});
 
 app.listen(PORT, console.log(`Server added on PORT ${PORT}`));
